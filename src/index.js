0a8e92333dfa483b86f98fccaca1da253485ce34
Elise Maschke <elise.maschke@outlook.de>
2018-11-08 09:45:18 +0100

    First female version

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import Popper from "popper.js";
+
+require("bootstrap");
+
+/*
+A small exercise:
+1. Create a class to access the "Dicebar Avatars API": https://avatars.dicebear.com/
+2. Create a small website with a button to generate 3 random avatars each time the button is clicked or the page is reloaded (edited)
+Getting the "uuid" package up and running:
+1. Run `npm install uuid`
+2. Load it in your JavaScript file (for example index.js): `const uuid = require("uuid/v4");`
+You can call it then like this:
+```uuid();```
+*/
+
+// [ X ] Create a button and an empty div for the coming images.
+// [ X ] Install and use uuid.
+// [   ] Create the class for the Api.
+
+("use strict");
+
+class AvatarApi {
+  // Setting up the properties, we need in our class
+  constructor() {
+    this.apiUrl = "https://avatars.dicebear.com/v2";
+    this.genderEndpoint = "/:sprites";
+    this.codeEndpoint = "/:seed.svg";
+
+    // Registering the form events
+    this.registerEvents();
+  }
+
+  registerEvents() {
+    // Get the needed elements
+    const button = document.querySelector(".btn");
+
+    button.addEventListener("click", e => {
+      console.log(e.target);
+
+      const urls = this.getAvatars();
+
+      // Update the div element to have the new text inside
+      document.querySelector("#avatars").innerHTML = `
+      <img src="${urls[0]}" alt="Avatar" width="30%">
+        <img src="${urls[1]}" alt="Avatar" width="30%">
+        <img src="${urls[2]}" alt="Avatar" width="30%">`;
+    });
+  }
+
+  getAvatars() {
+    const url1 = `${this.apiUrl}${this.getURL()}`;
+    const url2 = `${this.apiUrl}${this.getURL()}`;
+    const url3 = `${this.apiUrl}${this.getURL()}`;
+
+    console.log("get data", url1, url2, url3);
+
+    return [url1, url2, url3];
+  }
+
+  getURL() {
+    const uuid = require("uuid/v4");
+    const endpointOne = this.genderEndpoint.replace(":sprites", "female");
+    const endpointTwo = this.codeEndpoint.replace(":seed", uuid());
+    const endpoint = `${endpointOne}${endpointTwo}`;
+
+    return endpoint;
+  }
+}
+
+const avatarApi = new AvatarApi();
+console.log(avatarApi);
